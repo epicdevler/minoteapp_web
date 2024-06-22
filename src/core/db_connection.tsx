482013363856d004cc5d4e3a4d3f7c5c4328eb0a
epicdevler <dev.epicdevler@gmail.com>
2024-06-22 21:02:20 +0100

    fixed issues with env keys not being referenced

--- src/core/db_connection.tsx
+++ src/core/db_connection.tsx
@@ -1,5 +1,5 @@
 import * as Realm from 'realm-web'
 
 export const db_connection = () => {
-    return Realm.getApp(process.env.NEXT_PUBLIC_APP_ID)
+    return Realm.getApp(process.env.NEXT_PUBLIC_APP_ID!!)
 }
